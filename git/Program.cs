978cd6c94fc2019117f535fad929410543fa3257
SamiraShiralieva <41107094+SamiraShiralieva@users.noreply.github.com>
2018-09-12 13:11:41 +0300

    add multiplication and division operations

--- git/Program.cs
+++ git/Program.cs
@@ -30,7 +30,15 @@ namespace lab1
             if (c == "2")
             {
                 Console.WriteLine("Результат вычитания = {0}", a - b);
-            }       
+            }  
+             if (c == "3")
+            {
+                Console.WriteLine("Результат умножения = {0}", a * b);
+            }
+            if (c == "4")
+            {
+                Console.WriteLine("Результат деления = {0}", a / b);
+            }     
         }
     }
 }
