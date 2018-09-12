6040b3c0f393403323b86c9cab78218e89725cf8
SamiraShiralieva <41107094+SamiraShiralieva@users.noreply.github.com>
2018-09-12 13:05:43 +0300

    add menu for Program.cs

---
+++ git/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Выберите операцию:
+            1 - Сложение 
+            2 - Вычитание 
+            3 - Умножение 
+            4 - Деление");           
+        }
+    }
+}
