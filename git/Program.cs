a928b498c4a780546f59d57c9fb1df472c7b82a8
SamiraShiralieva <41107094+SamiraShiralieva@users.noreply.github.com>
2018-09-13 19:53:13 +0300

    division operation is returned

--- git/Program.cs
+++ git/Program.cs
@@ -35,6 +35,10 @@ namespace lab1
             {
                 Console.WriteLine("Результат умножения = {0}", a * b);
             }
+            if (c == "4")
+            {
+                Console.WriteLine("Результат деления = {0}", a / b);
+            }
                 
         }
     }
